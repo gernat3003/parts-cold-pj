70a8480f6eefbc8de5bd7360252377340b910468
gersar08 <nagerman26@gmail.com>
2024-03-24 23:03:04 -0600

    Fix a vulnerability with Routes

--- parts-cold-project/src/App.js
+++ parts-cold-project/src/App.js
@@ -1,16 +1,18 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
   useNavigate,
+  useLocation
 } from "react-router-dom";
 import './App.css';
 import Login from './components/Login';
 import Header from './components/Header';
 import PropTypes from 'prop-types'
 import MainDashboard from './components/MainDashboard';
+import HeaderLogin from './components/HeaderLogin';
 
 function ProtectedComponent({ children }) {
   const navigate = useNavigate();
@@ -29,19 +31,25 @@ ProtectedComponent.propTypes = {
   children: PropTypes.node,
 };
 
+function HeaderSelector() {
+  const location = useLocation();
+
+  return location.pathname === "/login" ? <HeaderLogin /> : <Header />;
+}
+
 function App() {
   return (
 
     <Router>
       <div className="bg-slate-700 h-screen flex flex-col overflow-hidden">
         <header className="bg-white"  >
-          <Header />
+          <HeaderSelector />
         </header>
         <div className="App-body">
           <Routes>
             <Route path="/" element={<Navigate to="/login" />} />
             <Route path="/login" element={<Login />} exactpath />
-            <Route path='/maindashboard' element={<MainDashboard /> } />
+            <Route path='/maindashboard' element={<MainDashboard />} />
             <Route path="*" element={<h1>Not Found 404</h1>} />
           </Routes>
         </div>
