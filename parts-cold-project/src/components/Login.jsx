ff0af5c8e85dd71288e0261e94872786da280240
gersar08 <nagerman26@gmail.com>
2024-03-24 22:53:53 -0600

    Create a Login Component

--- parts-cold-project/src/components/Login.jsx
+++ parts-cold-project/src/components/Login.jsx
@@ -1,7 +1,118 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const apiUrl =
+    process.env.NODE_ENV === "production"
+      ? process.env.REACT_APP_API_URL_PROD
+      : process.env.REACT_APP_API_URL_DEV;
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const csrfResponse = await fetch(
+        `${apiUrl}/sanctum/csrf-cookie`, //cambiar direccion api
+        {
+          credentials: "include",
+        }
+      );
+      if (!csrfResponse.ok) {
+        throw new Error("Error en la autenticación");
+      }
+
+      const response = await fetch(`${apiUrl}/api/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        credentials: "include",
+        body: JSON.stringify({ user_name: username, password }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Error en la autenticación");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      localStorage.setItem("token", data.token);
+      localStorage.setItem("user", username);
+      localStorage.setItem("userIdLogged", data.user.id);
+      navigate("/maindashboard");
+    } catch (error) {
+      console.error(
+        "There was a problem with the fetch operation: " + error.message
+      );
+      toast.error(error.message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      }); // Actualizamos el estado error con el mensaje de error
+    }
+  };
   return (
-    <div></div>
+    <div className="bg-gray-100 rounded-lg py-10 px-4 lg:px-24">
+      <ToastContainer />
+      <p className="text-center text-sm bold text-gray-500 font-light">
+        Inicia Sesion para continuar
+      </p>
+      <form className="mt-6" onSubmit={handleSubmit}>
+        <div className="relative">
+          <input
+            className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
+            id="username"
+            type="text"
+            required
+            placeholder="Usuario"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <div className="absolute left-0 inset-y-0 flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7 ml-3 text-gray-400 p-1"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+              <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+            </svg>
+          </div>
+        </div>
+        <div className="relative mt-3">
+          <input
+            className="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline"
+            id="password"
+            required
+            type="password"
+            placeholder="Contraseña"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div className="absolute left-0 inset-y-0 flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-7 w-7 ml-3 text-gray-400 p-1"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z" />
+            </svg>
+          </div>
+        </div>
+        <div className="flex items-center justify-center mt-8">
+          <button className="text-white py-2 px-4 uppercase rounded bg-indigo-500 hover:bg-indigo-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
+            Iniciar Sesion
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
