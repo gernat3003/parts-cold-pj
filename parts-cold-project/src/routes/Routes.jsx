ede8a4fb42d5523b66903485e66c30bbdc0aec14
gersar08 <nagerman26@gmail.com>
2024-04-06 11:30:56 -0600

    Update Routes 🚀

--- parts-cold-project/src/routes/Routes.jsx
+++ parts-cold-project/src/routes/Routes.jsx
@@ -1,18 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
   useNavigate,
-  useLocation
+  useLocation,
 } from "react-router-dom";
-import '../style/App.css'
-import PropTypes from 'prop-types'
-import HeaderLogin from './../components/HeaderLogin';
-import Header from './../components/Header'
-import Login from '../pages/Login';
-import MainDashboard from './../pages/MainDashboard';
+import "../style/App.css";
+import PropTypes from "prop-types";
+import HeaderLogin from "./../components/HeaderLogin";
+import Header from "./../components/Header";
+import Login from "../pages/Login";
+import MainDashboard from "./../pages/MainDashboard";
+import Users from "./../pages/Users";
+import Inventory from "./../pages/Inventory";
+import SalesRecord from "./../pages/SalesRecord";
+import Invoices from "./../pages/Invoices";
+import MarketCar from "../pages/MarketCar";
 
 function ProtectedComponent({ children }) {
   const navigate = useNavigate();
@@ -38,18 +43,22 @@ function HeaderSelector() {
 }
 
 function RoutesComp() {
- return (
-
+  return (
     <Router>
       <div className="bg-slate-700 h-screen flex flex-col overflow-hidden">
-        <header className="bg-white"  >
+        <header className="bg-white">
           <HeaderSelector />
         </header>
         <div className="App-body">
           <Routes>
             <Route path="/" element={<Navigate to="/login" />} />
             <Route path="/login" element={<Login />} exactpath />
-            <Route path='/maindashboard' element={<MainDashboard />} />
+            <Route path="/maindashboard" element={<MainDashboard />} />
+            <Route path="/maindashboard/users" element={<Users />} />
+            <Route path="/maindashboard/inventory" element={<Inventory />} />
+            <Route path="/maindashboard/record" element={<SalesRecord />} />
+            <Route path="/maindashboard/invoices" element={<Invoices />} />
+            <Route path="/maindashboard/marketcar" element={<MarketCar />} />
             <Route path="*" element={<h1>Not Found 404</h1>} />
           </Routes>
         </div>
