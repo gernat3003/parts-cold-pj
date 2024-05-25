062e2bf2b23fdfccaf3006ef925082c279a1ffd4
gersar08 <nagerman26@gmail.com>
2024-05-24 22:00:21 -0600

    Add funcionality to search bar

--- parts-cold-project/src/components/SearchBar.jsx
+++ parts-cold-project/src/components/SearchBar.jsx
@@ -1,23 +1,32 @@
+import { HomeIcon } from "@heroicons/react/24/outline";
 import React, { useState } from "react";
-import useGetRequest from '../Hooks/useGetRequest'; // Asegúrate de importar tu hook personalizado
 
-export default function SearchBar() {
+export default function SearchBar({ onSearch }) {
   const [terminoBusqueda, setTerminoBusqueda] = useState("");
-  const { data, error, loading, execute } = useGetRequest(
-    `http://localhost:8000/api/buscar?termino=${terminoBusqueda}`
-  );
 
   const buscar = () => {
-    execute();
+    onSearch(terminoBusqueda);
+  };
+
+  const regresar = () => {
+    setTerminoBusqueda("");
+    onSearch("");
   };
 
   return (
-    <div className="flex items-center justify-center ">
+    <div className="flex items-center justify-center">
       <div className="flex border-2 border-gray-200 rounded">
+        <button
+          className="px-4 text-white bg-gray-600 border-l"
+          onClick={regresar}
+          title="Regresar"
+        >
+          <HomeIcon className="w-6 h-6" />
+        </button>
         <input
           type="text"
           className="px-4 py-2 w-80"
-          placeholder="Search..."
+          placeholder="Busca por nombre o palabras clave en la descripción"
           value={terminoBusqueda}
           onChange={(e) => setTerminoBusqueda(e.target.value)}
         />
