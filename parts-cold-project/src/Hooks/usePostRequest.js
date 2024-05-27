5cc403db9ce249b56443787b30a98fc384345677
gersar08 <nagerman26@gmail.com>
2024-05-27 12:46:24 -0600

    Update post request

--- parts-cold-project/src/Hooks/usePostRequest.js
+++ parts-cold-project/src/Hooks/usePostRequest.js
@@ -1,44 +1,59 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// Creamos una función que devuelve una instancia de Axios configurada
 const createAxiosInstance = () => {
-    const apiUrl = process.env.NODE_ENV === "production"
-        ? process.env.REACT_APP_API_URL_PROD
-        : process.env.REACT_APP_API_URL_DEV;
+    const apiUrl = 'http://localhost:8000/api/'
 
     return axios.create({
-        baseURL: apiUrl, // URL base para todas las solicitudes
-        timeout: 5000, // Tiempo de espera para las solicitudes (en milisegundos)
+        baseURL: apiUrl,
+        timeout: 5000,
         headers: {
-            'Content-Type': 'application/json',
-            Accept: "application/json", // Tipo de contenido para las solicitudes
+            "Content-Type": "application/json",
+            Accept: "application/json",
         },
+        withCredentials: true,
     });
 };
 
-// Hook personalizado para manejar solicitudes POST
-export default function usePostRequest() {
-    const [loading, setLoading] = useState(false);
+const usePostRequest = (initialUrl, postData) => {
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-
-    const postData = async (url, data, token) => {
-        setLoading(true);
-        try {
-            const axiosInstance = createAxiosInstance();
-            await axiosInstance.post(url, data, {
-                headers: {
-                    'Authorization': `Bearer ${token}`
-                }
-            });
-            setLoading(false);
-        } catch (error) {
-            setError(error);
-            setLoading(false);
+    useEffect(() => {
+        if (!initialUrl) {
+            throw new Error("La URL inicial no puede estar vacía");
         }
-    };
 
-    return { postData, loading, error };
+        const axiosInstance = createAxiosInstance();
+        let source = axios.CancelToken.source();
+        const authToken = localStorage.getItem("token");
+
+        const fetchData = async () => {
+            try {
+                const response = await axiosInstance.post(initialUrl, postData, {
+                    headers: {
+                        Authorization: `Bearer ${authToken}`,
+                    },
+                    cancelToken: source.token
+                });
+                setData(response.data);
+            } catch (error) {
+                if (!axios.isCancel(error)) {
+                    setError(error);
+                }
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+
+        return () => {
+            source.cancel("Solicitud cancelada por limpieza");
+        };
+    }, [initialUrl]);
+
+    return { data, loading, error };
 };
 
-
+export default usePostRequest;
