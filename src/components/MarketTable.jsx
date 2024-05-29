76b39094f7d2ad5645bc32d8c3ecf386be9c0403
gersar08 <nagerman26@gmail.com>
2024-05-29 16:00:56 -0600

    Fix issue with the load image

--- src/components/MarketTable.jsx
+++ src/components/MarketTable.jsx
@@ -20,7 +20,7 @@ const MarketTable = ({
           >
             <div className="w-full md:w-1/3 bg-white grid place-items-center">
               <img
-                src={item.img_product}
+                src={`https://coldparts.online/${item.img_product}`}
                 alt="tailwind logo"
                 className="rounded-xl"
               />
