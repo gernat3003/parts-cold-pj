d87ea0d655b8554c49b368252069cad627d2b5b5
gersar08 <nagerman26@gmail.com>
2024-05-29 15:59:50 -0600

    Create new user component form

---
+++ src/components/CreateNewUser.jsx
@@ -0,0 +1,175 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import useAxios from "../Hooks/useAxios";
+import { toast, ToastContainer } from "react-toastify";
+export default function CreateNewUser() {
+  const [formData, setFormData] = useState({
+    name: "",
+    user_name: "",
+    password: "",
+    rol: "",
+  });
+  const navigate = useNavigate();
+  const { fetchData, error } = useAxios({});
+  const handleConfirmUser = async (e) => {
+    e.preventDefault();
+    console.log(formData);
+    try {
+      await fetchData({
+        url: "users",
+        method: "post",
+        data: formData,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+      navigate("/maindashboard/usuarios", {
+        state: { successMessage: "Usuario creado con éxito" },
+      });
+    } catch (error) {
+      toast.error(error);
+    }
+  };
+  const handleChange = async (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSelectChange = (e) => {
+    const { value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      rol: value,
+    }));
+  };
+
+  const handleCancel = () => {
+    navigate("/maindashboard/usuarios", { state: { successMessage: null } });
+  };
+
+  if (error) {
+    toast.error(error, {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  }
+  return (
+    <div className="bg-transparent flex items-center justify-center w-screen h-screen">
+      <ToastContainer />
+      <div className="bg-white p-12 rounded-lg shadow-lg max-w-sm w-full">
+        <div className="flex justify-center mb-6">
+          <span className="inline-block bg-gray-200 rounded-full p-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4"
+              />
+            </svg>
+          </span>
+        </div>
+        <h2 className="text-2xl font-semibold text-center mb-4">
+          Crea un nuevo usuario
+        </h2>
+        <p className="text-gray-600 text-center mb-6">
+          Ingresa los detalles del registro.
+        </p>
+        <form onSubmit={handleConfirmUser}>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-semibold mb-2">
+              Nombre Completo *
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              className="form-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:ring-blue-500"
+              required
+              placeholder="James Brown"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-semibold mb-2">
+              Usuario *
+            </label>
+            <input
+              type="username"
+              id="username"
+              name="user_name"
+              className="form-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:ring-blue-500"
+              required
+              placeholder="Usuario"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-gray-700 text-sm font-semibold mb-2">
+              Contraseña *
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="form-input w-full px-4 py-2 border rounded-lg text-gray-700 focus:ring-blue-500"
+              required
+              placeholder="••••••••"
+              name="password"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-semibold mb-2"
+              onChange={handleChange}
+            >
+              Rol *
+            </label>
+            <select
+              name="selectedRol"
+              className="w-full pl-2 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none focus:border-indigo-500"
+              onChange={handleSelectChange}
+              required
+            >
+              <option value="">Seleccione un rol</option>
+              <option value="admin">Administrador</option>
+              <option value="Operador de Produccion">
+                Operador de Produccion
+              </option>
+              <option value="Supervisor de Inventario">
+                Supervisor de Inventario
+              </option>
+              <option value="Operador de caja">Operador de caja</option>
+            </select>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full mb-3 bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-opacity-50"
+          >
+            Registrar
+          </button>
+          <button
+            onClick={handleCancel}
+            type="back"
+            className="w-full bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-opacity-50"
+          >
+            Cancelar
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
