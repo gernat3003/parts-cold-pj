8f4790a4c3813bf3f827b49e664e910b1c6db5b9
gersar08 <nagerman26@gmail.com>
2024-05-30 12:18:46 -0600

    Refactor axios

--- src/Hooks/useAxios.js
+++ src/Hooks/useAxios.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 
 const useAxios = (initialConfig) => {
@@ -10,41 +10,44 @@ const useAxios = (initialConfig) => {
   axios.defaults.withCredentials = true;
   axios.defaults.withXSRFToken = true;
 
-  const BASE_URL = "https://coldparts.online/api/"; // URL base por defecto
+  const BASE_URL = "http://localhost:8000/api/";
 
-  const fetchData = async (overrideConfig = {}) => {
-    setLoading(true);
-    setError('');
-    try {
-      const res = await axios({
-        ...config,
-        ...overrideConfig,
-        url: `${BASE_URL}${overrideConfig.url || config.url}`, // Concatenar la URL base con la URL de la configuración
-        headers: {
-          ...config.headers,
-          ...overrideConfig.headers,
-          'Authorization': `Bearer ${localStorage.getItem('token')}`,
-        },
-        withCredentials: true,
-        data: overrideConfig.data || config.data, // Asegúrate de que estás pasando los datos aquí
-        maxContentLength: 50 * 1024 * 1024, // 50MB
-        maxBodyLength: 50 * 1024 * 1024, // 50MB
-      });
-      setResponse(res.data);
-    } catch (err) {
-      setError(err.message || 'ocurrio un error');
-    } finally {
-      setLoading(false);
-    }
-  };
+  const fetchDataRef = useRef(null);
 
   useEffect(() => {
+    fetchDataRef.current = async (overrideConfig = {}) => {
+      setLoading(true);
+      setError('');
+      console.log(overrideConfig)
+      try {
+        const res = await axios({
+          ...config,
+          ...overrideConfig,
+          url: `${BASE_URL}${overrideConfig.url || config.url}`,
+          headers: {
+            ...config.headers,
+            ...overrideConfig.headers,
+            'Authorization': `Bearer ${localStorage.getItem('token')}`,
+          },
+          withCredentials: true,
+          data: overrideConfig.data || config.data,
+          maxContentLength: 50 * 1024 * 1024,
+          maxBodyLength: 50 * 1024 * 1024,
+        });
+        setResponse(res.data);
+      } catch (err) {
+        setError(err.message || 'ocurrio un error');
+      } finally {
+        setLoading(false);
+      }
+    };
+
     if (config.url && config.method) {
-      fetchData({});
+      fetchDataRef.current({});
     }
   }, [config]);
 
-  return { response, error, loading, setConfig, fetchData };
+  return { response, error, loading, setConfig, fetchData: fetchDataRef.current };
 };
 
 export default useAxios;
