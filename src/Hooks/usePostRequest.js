c429b6285390f2d0b9af2e16c047b9fd93b7409d
gersar08 <nagerman26@gmail.com>
2024-05-30 00:26:31 -0600

    fix url

--- src/Hooks/usePostRequest.js
+++ src/Hooks/usePostRequest.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const createAxiosInstance = () => {
-    const apiUrl = "https://coldparts.online/api"
+    const apiUrl = "https://coldparts.online/api/"
 
     return axios.create({
         baseURL: apiUrl,
