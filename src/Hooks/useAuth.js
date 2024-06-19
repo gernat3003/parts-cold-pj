9d8d219b90469d37d7da3c39a38559d2d50a12c8
gersar08 <nagerman26@gmail.com>
2024-06-19 17:19:10 -0600

    Create a useAuth hook

---
+++ src/Hooks/useAuth.js
@@ -0,0 +1,34 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const useAuth = () => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    if (token) {
+      // Verificar la validez del token con el servidor
+      axios.get('http://localhost:8000/api/verify', {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      .then(response => {
+        // Si el token es válido, puedes continuar o manejar la respuesta
+      })
+      .catch(error => {
+        // Si hay un error (ej. token no válido), redirigir a login
+        console.error("Authentication error", error);
+        navigate("/login");
+      });
+    } else {
+      // Si no hay token, redirigir a login
+      console.error("No token found");
+      navigate("/login");
+    }
+  }, [navigate, token]);
+
+};
+
+export default useAuth;
