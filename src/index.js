3ef6cc219c814df7c5a34f5abc236fa14c614cc4
gersar08 <nagerman26@gmail.com>
2024-08-26 22:29:06 -0600

    Add an Error Boundary in index file and delete restrict mode

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import "react-toastify/dist/ReactToastify.css";
 import RoutesComp from './routes/Routes';
-
+import ErrorBoundary from './utils/ErrorBoundary';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ErrorBoundary>
     <RoutesComp />
-  </React.StrictMode>
+  </ErrorBoundary>
 );
 
